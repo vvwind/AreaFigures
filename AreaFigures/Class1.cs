b7f860e96c2727cf6869756ec429fbcabf68fc04
vvwind <vladimirwind@yahoo.com>
2022-08-08 01:05:19 +0300

    Добавьте файлы проекта.

---
+++ AreaFigures/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace AreaFigures
+{
+    public abstract class Shape
+    {
+        public abstract double Area();
+    }
+
+    public class Triangle : Shape
+    {
+        public double Side_A { get; set; }
+        public double Side_B { get; set; }
+        public double Side_C { get; set; }
+
+
+        public override double Area()
+        {
+
+            var sides_array = new double[] { Side_A, Side_B, Side_C };
+            Array.Sort(sides_array);
+            var gerone = (Side_A + Side_B + Side_C) / 2;
+
+            if ((Math.Pow(sides_array[0], 2) + Math.Pow(sides_array[1], 2)) == (Math.Pow(sides_array[2], 2)))
+            {
+                return (sides_array[0] * sides_array[1]) / 2;
+            }
+            else
+            {
+                return Math.Sqrt(gerone * (gerone - Side_A) * (gerone - Side_B) * (gerone - Side_C));
+            }
+
+        }
+    }
+
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public override double Area()
+        {
+
+            return (Math.Pow(Radius, 2) * Math.PI);
+        }
+    }
+}
+
+
+
+
